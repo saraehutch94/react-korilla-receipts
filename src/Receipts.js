7079c25ed7812ef0966af5cf0105fcba1636ad70
Sara Hutchinson <84148986+saraehutch94@users.noreply.github.com>
2022-01-05 12:08:18 -0500

    map through receipts array, return individual Receipt components with proper props

--- src/Receipts.js
+++ src/Receipts.js
@@ -2,10 +2,21 @@ import React from "react";
 import Receipt from "./Receipt";
 
 function Receipts(props) {
+  const mappedReceipts = props.receipts.map((object, index) => {
+    return (
+      <Receipt
+        key={index}
+        order={object.order}
+        paid={object.paid}
+        person={object.person}
+      />
+    );
+  });
+
   return (
     <>
       <div>Hello Receipts</div>
-      <Receipt />
+      <div>{mappedReceipts}</div>
     </>
   );
 }
