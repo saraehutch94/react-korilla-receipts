815a57fedbcd374c51b27afa544acf1ac5de6788
Sara Hutchinson <84148986+saraehutch94@users.noreply.github.com>
2022-01-05 11:54:07 -0500

    import Receipts component to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import receipts from "./receiptData";
+import Receipts from "./Receipts";
 import "./styles.css";
 
 export default function App() {
+  const [receipt, setReceipt] = useState(receipts);
   return (
     <div className="App">
       <h1>Korilla Receipts</h1>
+      <Receipts />
     </div>
   );
 }
---
+++ src/Receipts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Receipts() {
+  return <div>Hello Receipts</div>;
+}
+
+export default Receipts;
