ccb4df2e7fc16934cf45408ac3cc6060c160a9c1
Sara Hutchinson <84148986+saraehutch94@users.noreply.github.com>
2022-01-05 12:29:30 -0500

    render individual receipt data in Receipt component

--- src/Receipt.js
+++ src/Receipt.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Receipt(props) {
-  return <div>hello one receipt</div>;
+  return (
+    <>
+      <div>Customer: {props.person}</div>
+      <ul>
+        Order:
+        <li>{props.order.main}</li>
+        <li>{props.order.protein}</li>
+        <li>{props.order.rice}</li>
+        <li>{props.order.sauce}</li>
+      </ul>
+      <div>Total: {props.order.cost}</div>
+      <div>Paid: {String(props.paid)}</div>
+    </>
+  );
 }
 
 export default Receipt;
