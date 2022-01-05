891759d1766268aa6e02efa826b6639cf44c9be0
Sara Hutchinson <84148986+saraehutch94@users.noreply.github.com>
2022-01-05 13:23:27 -0500

    continue with Form component state

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
+  const [formData, setFormData] = useState("");
+
+  function handleChange(e) {
+    setFormData(e.target.value);
+  }
+
+  console.log(formData);
+
   return (
     <form>
       <label>
         <span>Search for receipt by name</span>
+        <input value={formData} onChange={handleChange} />
       </label>
+      <button>SEARCH</button>
     </form>
   );
 }
