51db9ee889d0f4c65845317ac27b665d3d4f1552
Sara Hutchinson <84148986+saraehutch94@users.noreply.github.com>
2022-01-05 12:02:11 -0500

    import Receipt component to Receipts component

--- src/Receipts.js
+++ src/Receipts.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Receipt from "./Receipt";
 
 function Receipts(props) {
-  return <div>Hello Receipts</div>;
+  return (
+    <>
+      <div>Hello Receipts</div>
+      <Receipt />
+    </>
+  );
 }
 
 export default Receipts;
