d99866d155a2a7e034377fdfadc86ebcb75d5ea2
Sara Hutchinson <84148986+saraehutch94@users.noreply.github.com>
2022-01-05 13:41:50 -0500

    continued: form logic w state

--- src/Form.js
+++ src/Form.js
@@ -3,14 +3,16 @@ import React, { useState } from "react";
 function Form() {
   const [formData, setFormData] = useState("");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   function handleChange(e) {
     setFormData(e.target.value);
   }
 
-  console.log(formData);
-
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         <span>Search for receipt by name</span>
         <input value={formData} onChange={handleChange} />
